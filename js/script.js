4531bd68c72cb7e4bc4a77a93f2f24e14505b0bc
北邙 <communism_1921@163.com>
2023-11-01 15:55:57 +0800

    2023-11-01 15:55:54

--- js/script.js
+++
@@ -1,9 +0,0 @@
-const scaleSlider = document.getElementById('scaleSlider');
-const scaledDiv = document.getElementById('scaledDiv');
-
-scaleSlider.addEventListener('input', (event) => {
-  const scaleValue = event.target.value;
-  scaledDiv.style.transform = `scale(${scaleValue})`;
-  const containerWidth = scaledDiv.clientWidth * scaleValue;
-  scaledDiv.style.width = containerWidth + 'px';
-});
