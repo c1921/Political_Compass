5be49760a6682c3ea038805576a4b73d73ca0a16
北邙 <communism_1921@163.com>
2023-11-01 15:45:50 +0800

    2023-11-01 15:45:47

--- js/script.js
+++ js/script.js
@@ -1,46 +1,9 @@
-var container = document.getElementById('scroll');
-var image = document.getElementById('container');
+const scaleSlider = document.getElementById('scaleSlider');
+const scaledDiv = document.getElementById('scaledDiv');
 
-var startX, startY, pinchStartDistance;
-
-container.addEventListener('touchstart', function (e) {
-e.preventDefault();
-
-if (e.touches.length === 1) {
-startX = e.touches[0].pageX;
-startY = e.touches[0].pageY;
-} else if (e.touches.length === 2) {
-pinchStartDistance = getPinchDistance(e.touches[0], e.touches[1]);
-}
+scaleSlider.addEventListener('input', (event) => {
+  const scaleValue = event.target.value;
+  scaledDiv.style.transform = `scale(${scaleValue})`;
+  const containerWidth = scaledDiv.clientWidth * scaleValue;
+  scaledDiv.style.width = containerWidth + 'px';
 });
-
-container.addEventListener('touchmove', function (e) {
-e.preventDefault();
-
-if (e.touches.length === 1) {
-var offsetX = e.touches[0].pageX - startX;
-var offsetY = e.touches[0].pageY - startY;
-
-container.scrollLeft -= offsetX;
-container.scrollTop -= offsetY;
-
-startX = e.touches[0].pageX;
-startY = e.touches[0].pageY;
-} else if (e.touches.length === 2) {
-var pinchDistance = getPinchDistance(e.touches[0], e.touches[1]);
-var scale = pinchDistance / pinchStartDistance;
-
-pinchStartDistance = pinchDistance;
-
-var currentWidth = parseFloat(getComputedStyle(image).width);
-var newWidth = currentWidth * scale;
-
-image.style.width = newWidth + 'px';
-}
-});
-
-function getPinchDistance(touch1, touch2) {
-var dx = touch1.pageX - touch2.pageX;
-var dy = touch1.pageY - touch2.pageY;
-return Math.sqrt(dx * dx + dy * dy);
-}
